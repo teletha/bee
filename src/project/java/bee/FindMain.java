3bc6c2a86a68e4df2c016d9f012db02b24f3064b
Teletha <Teletha.T@gmail.com>
2012-04-19 12:47:02 +0900

    CommandLineUserInterface display task name well when some task show no message.

---
+++ src/project/java/bee/FindMain.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2012 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package bee;
+
+/**
+ * @version 2012/04/19 12:40:04
+ */
+public class FindMain extends bee.task.FindMain {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String main() {
+        return BeeInstaller.class.getName();
+    }
+}
