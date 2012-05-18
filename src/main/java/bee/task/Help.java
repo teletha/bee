638938e7c29778bcc57c83bd8ac505145b807a90
Teletha <Teletha.T@gmail.com>
2012-05-18 17:01:24 +0900

    Implement help task.

---
+++ src/main/java/bee/task/Help.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package bee.task;
+
+import java.util.Locale;
+
+import bee.Platform;
+
+/**
+ * @version 2012/05/18 11:11:39
+ */
+public class Help extends Task {
+
+    @Command("Display environment.")
+    public void version() {
+        ui.talk("Bee version: 0.1");
+        ui.talk("Java version: ", System.getProperty("java.version"), " by ", System.getProperty("java.vendor"));
+        ui.talk("Java home: ", Platform.JavaHome);
+        ui.talk("Locale: ", Locale.getDefault().getDisplayName());
+        ui.talk("Encoding: ", Platform.Encoding.name());
+    }
+}
