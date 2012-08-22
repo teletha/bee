9226cfcdbd6ef6ead6f72ff62dd73757cd42dd36
Teletha <Teletha.T@gmail.com>
2012-08-22 17:04:00 +0900

    Sync.

--- src/main/java/bee/license/MIT.java
+++ src/main/java/bee/license/MIT.java
@@ -9,6 +9,9 @@
  */
 package bee.license;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @version 2012/01/27 0:55:50
  */
@@ -18,11 +21,28 @@ public class MIT implements License {
      * {@inheritDoc}
      */
     @Override
-    public String text() {
-        return "" + //
-        "Copyright (c) <year> <copyright holders>" + //
-        "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:" + //
-        "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software." + //
-        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+    public List<String> text() {
+        List<String> license = new ArrayList();
+        license.add("Copyright (c) <YEAR> <NAME>");
+        license.add("");
+        license.add("Permission is hereby granted, free of charge, to any person obtaining a copy of");
+        license.add("this software and associated documentation files (the \"Software\"), to deal in");
+        license.add(" the Software without restriction, including without limitation the rights to use, copy,");
+        license.add("modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,");
+        license.add("and to permit persons to whom the Software is furnished to do so, subject to the");
+        license.add("following conditions:");
+        license.add("");
+        license.add("The above copyright notice and this permission notice shall be included in all copies");
+        license.add("or substantial portions of the Software.");
+        license.add("");
+        license.add("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS");
+        license.add("OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
+        license.add(" FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL");
+        license.add("THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR");
+        license.add("OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,");
+        license.add("ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR");
+        license.add("OTHER DEALINGS IN THE SOFTWARE.");
+
+        return license;
     }
 }
