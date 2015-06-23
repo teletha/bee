437522a86555c8e2af8a778943700c8d2f220f8d
Teletha <Teletha.T@gmail.com>
2015-06-24 02:23:58 +0900

    Add GitHub.

---
+++ src/main/java/bee/task/Github.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package bee.task;
+
+import bee.api.Task;
+
+/**
+ * @version 2015/06/23 13:33:31
+ */
+public class Github extends Task {
+    //
+    // /** The github repository id. */
+    // private final RepositoryId id = new RepositoryId("Teletha", project.getProduct());
+    //
+    // /** The github repository operator. */
+    // private final RepositoryService service = new RepositoryService();
+    //
+    // /**
+    // * <p>
+    // * Create GitHub repository.
+    // * </p>
+    // *
+    // * @throws IOException
+    // */
+    // @Command("Generate GitHub repository.")
+    // public void release() throws Exception {
+    //
+    // RepositoryTag current = findCurrentRelease();
+    //
+    // if (current == null) {
+    // // create current release
+    // }
+    // }
+    //
+    // /**
+    // * <p>
+    // * Find the current release tag.
+    // * </p>
+    // *
+    // * @return
+    // * @throws Exception
+    // */
+    // private RepositoryTag findCurrentRelease() throws Exception {
+    // List<RepositoryTag> tags = service.getTags(id);
+    //
+    // for (RepositoryTag tag : tags) {
+    // if (tag.getName().equals(project.getVersion())) {
+    // return tag;
+    // }
+    // }
+    // return null;
+    // }
+}
