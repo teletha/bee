8bf8ea459f14a513f1240149b0cc54b86b7b7a62
Teletha <Teletha.T@gmail.com>
2016-10-15 22:06:44 +0900

    Implements install:jar command.

--- src/main/java/bee/task/Install.java
+++ src/main/java/bee/task/Install.java
@@ -9,7 +9,10 @@
  */
 package bee.task;
 
+import java.nio.file.Path;
+
 import bee.api.Command;
+import bee.api.Project;
 import bee.api.Repository;
 import bee.api.Task;
 import kiss.I;
@@ -19,12 +22,37 @@ import kiss.I;
  */
 public class Install extends Task {
 
-    @Command("Install project into the local repository.")
+    @Command(defaults = true, value = "Install project into the local repository.")
     public void project() {
         require(Test.class).test();
         require(Jar.class).source();
 
         Repository repository = I.make(Repository.class);
         repository.install(project);
+    }
+
+    @Command("Install local jar file into the local repository.")
+    public void jar() {
+        Path selected = ui.ask("Select a jar file to install.", I.walk(project.getRoot(), "*.jar"));
+        String group = ui.ask("Input group name.", selected.getFileName().toString());
+        String product = ui.ask("Input product name.", group);
+        String version = ui.ask("Input product version.", "1.0");
+
+        Repository repository = I.make(Repository.class);
+        repository.install(new TemporaryProject(group, product, version), selected);
+    }
+
+    /**
+     * @version 2016/10/15 21:13:19
+     */
+    private static class TemporaryProject extends Project {
+
+        /**
+         * 
+         */
+        private TemporaryProject(String group, String product, String version) {
+            product(group, product, version);
+        }
+
     }
 }
