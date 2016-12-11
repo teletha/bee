9de00a9e5d68150d8748bdcbb55e5c69bdd3d084
Teletha <Teletha.T@gmail.com>
2016-12-11 12:12:27 +0900

    progress...

--- src/main/java/bee/task/EnhanceLibrary.java
+++ src/main/java/bee/task/EnhanceLibrary.java
@@ -9,22 +9,28 @@
  */
 package bee.task;
 
+import java.io.IOException;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map.Entry;
 
 import bee.Platform;
 import bee.api.Command;
 import bee.api.Scope;
 import bee.api.Task;
 import bee.util.PathSet;
+import bee.util.Paths;
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.ClassWriter;
 import kiss.Events;
 import kiss.I;
 import kiss.Table;
-import kiss.ThrowableFunction;
+import lombok.SneakyThrows;
 import lombok.Value;
 import lombok.experimental.Accessors;
 
@@ -40,29 +46,80 @@ public class EnhanceLibrary extends Task {
      * 
      * @return A path to enhanced class library.
      */
+    @SneakyThrows
     @Command(value = "Enhance external class.", defaults = true)
     public Path enhance() {
-        searchExtensionLibrary();
+        Path local = Paths.createDirectory(project.getOutput().resolve("enhance-library"));
+
+        for (Entry<Path, List<ExtensionDefinition>> entry : searchExtensionLibrary().entrySet()) {
+            // copy original archive
+            Path original = entry.getKey();
+            Path enhanced = local.resolve(original.getFileName());
+            I.copy(original, enhanced);
+            Path jar = jar(enhanced);
+
+            Table<Class, ExtensionDefinition> table = Events.from(entry.getValue()).toTable(ExtensionDefinition::target);
+
+            for (Entry<Class, List<ExtensionDefinition>> types : table.entrySet()) {
+                Path classFile = jar.resolve(types.getKey().getName().replace('.', '/') + ".class");
+
+                // start writing byte code
+                ClassReader reader = new ClassReader(Files.newInputStream(classFile));
+                ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+                reader.accept(writer, 0);
+
+                // retrieve byte code
+                byte[] bytes = writer.toByteArray();
+
+                // I.copy(new ByteArrayInputStream(bytes), Files.newOutputStream(classFile), true);
+            }
+        }
         return null;
     }
+
+    // public static void main(String[] args) throws Throwable {
+    // Map<String, String> env = new HashMap<>();
+    // env.put("create", "true");
+    // // locate file system by using the syntax
+    // // defined in java.net.JarURLConnection
+    // URI uri = URI.create("jar:file:/codeSamples/zipfs/zipfstest.zip");
+    //
+    // try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+    // Path externalTxtFile = Paths.get("/codeSamples/zipfs/SomeTextFile.txt");
+    // Path pathInZipfile = zipfs.getPath("/SomeTextFile.txt");
+    // // copy a file into the zip file
+    // Files.copy(externalTxtFile, pathInZipfile, StandardCopyOption.REPLACE_EXISTING);
+    // }
+    // }
 
     /**
      * <p>
      * Search all extension libraries.
      * </p>
      */
-    private void searchExtensionLibrary() {
-        Table<String, ExtensionDefinition> map = Events.from(project.getDependency(Scope.Compile))
+    private Table<Path, ExtensionDefinition> searchExtensionLibrary() {
+        return Events.from(project.getDependency(Scope.Compile))
                 .map(lib -> lib.getJar())
-                .map(path -> FileSystems.newFileSystem(path, ClassLoader.getSystemClassLoader()).getPath("/"))
+                .map(this::jar)
                 .startWith(project.getSources().resolve("resources"))
                 .map(path -> path.resolve("META-INF/services/" + EnhanceLibrary.class.getName()))
                 .take(Files::exists)
                 .flatIterable(Files::readAllLines)
                 .map(ExtensionDefinition::of)
-                .toTable(ExtensionDefinition::targetClass, ThrowableFunction.identity());
+                .toTable(ExtensionDefinition::archive);
+    }
 
-        System.out.println(map);
+    /**
+     * <p>
+     * Resolve jar file.
+     * </p>
+     * 
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    private Path jar(Path path) throws IOException {
+        return FileSystems.newFileSystem(path, ClassLoader.getSystemClassLoader()).getPath("/");
     }
 
     /**
@@ -72,7 +129,9 @@ public class EnhanceLibrary extends Task {
     @Accessors(fluent = true)
     private static class ExtensionDefinition {
 
-        String targetClass;
+        Class target;
+
+        Path archive;
 
         String extensionClass;
 
@@ -81,11 +140,14 @@ public class EnhanceLibrary extends Task {
         /**
          * @param line
          * @return
+         * @throws ClassNotFoundException
          */
-        private static ExtensionDefinition of(String line) {
+        static ExtensionDefinition of(String line) throws ClassNotFoundException {
             String[] values = line.split(" ");
+            Class target = Class.forName(values[0]);
+            Path archive = I.locate(target);
 
-            return new ExtensionDefinition(values[0], values[1], values[2]);
+            return new ExtensionDefinition(target, archive == null ? Platform.JavaRuntime : archive, values[1], values[2]);
         }
     }
 
