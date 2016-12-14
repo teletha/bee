f213b90f24b7dc5443aa555318bad46165943326
Teletha <Teletha.T@gmail.com>
2016-12-14 16:47:47 +0900

    FIX: Test task is broken.

--- src/main/java/bee/util/Java.java
+++ src/main/java/bee/util/Java.java
@@ -282,7 +282,7 @@ public class Java {
          */
         public static void main(String[] args) throws Exception {
             // load stacktrace codec
-            I.load(Java.class, false);
+            I.load(Java.class, true);
 
             // execute main process
             JVM vm = (JVM) I.make(Class.forName(args[2]));
