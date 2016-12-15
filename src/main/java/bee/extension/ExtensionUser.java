03537ba8efe3dc7e2cab41b199f04a12028b25e7
Teletha <Teletha.T@gmail.com>
2016-12-15 16:09:01 +0900

    Add sample and test.

---
+++ src/main/java/bee/extension/ExtensionUser.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package bee.extension;
+
+import java.util.Arrays;
+
+/**
+ * @version 2016/12/15 14:31:56
+ */
+public class ExtensionUser {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList("o", "k").join("").equals("ok"));
+    }
+}
