f6f35b0c2b698af1759c7bc9f9182f5101ad8085
Teletha <Teletha.T@gmail.com>
2018-04-04 11:38:39 +0900

    Update libraries.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -21,29 +21,29 @@ public class Project extends bee.api.Project {
 
     String AetherGroup = "org.eclipse.aether";
 
-    String AetherVersion = "1.1.0";
+    String AetherVersion = "[1.1.0,)";
 
-    String ByteBuddyVersion = "1.8.1";
+    String ByteBuddyVersion = "[1.8.0,)";
 
     {
         product(Bee.TOOL.getGroup(), Bee.TOOL.getProduct(), Bee.TOOL.getVersion());
         producer("Nameless Production Committee");
         describe("Task based project builder for Java");
 
-        require("com.github.teletha", "sinobu", "1.0");
-        require("com.github.teletha", "filer", "0.5");
-        require("com.github.teletha", "antibug", "0.6").atTest();
-        require("net.bytebuddy", "byte-buddy", ByteBuddyVersion);
-        require("net.bytebuddy", "byte-buddy-agent", ByteBuddyVersion);
         require(AetherGroup, "aether-api", AetherVersion);
         require(AetherGroup, "aether-util", AetherVersion);
         require(AetherGroup, "aether-impl", AetherVersion);
         require(AetherGroup, "aether-connector-basic", AetherVersion);
         require(AetherGroup, "aether-transport-http", AetherVersion);
-        require("org.junit.platform", "junit-platform-launcher", "1.1.0");
         require("org.apache.maven", "maven-aether-provider", "3.3.9");
+        require("net.bytebuddy", "byte-buddy", ByteBuddyVersion);
+        require("net.bytebuddy", "byte-buddy-agent", ByteBuddyVersion);
+        require("org.junit.platform", "junit-platform-launcher", "1.1.0");
         require("org.slf4j", "slf4j-nop", "1.8.0-beta2");
         require("org.slf4j", "jcl-over-slf4j", "1.8.0-beta2");
+        require("com.github.teletha", "sinobu", "1.0");
+        require("com.github.teletha", "filer", "0.5");
+        require("com.github.teletha", "antibug", "0.6").atTest();
 
         unrequire("org.codehaus.plexus", "plexus-classworlds");
         unrequire("org.codehaus.plexus", "plexus-component-annotations");
