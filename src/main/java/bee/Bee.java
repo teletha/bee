c5dd19b6cf1dfd8764a0cdf5a643be63defdd56e
Teletha <Teletha.T@gmail.com>
2019-02-15 18:03:37 +0900

    Revert.

--- src/main/java/bee/Bee.java
+++ src/main/java/bee/Bee.java
@@ -336,7 +336,7 @@ public class Bee {
     public static void main(String[] tasks) {
         if (tasks == null || tasks.length == 0) {
             Bee bee = new Bee();
-            bee.execute("bintray");
+            bee.execute("install");
         } else {
             Bee bee = new Bee();
             bee.execute(tasks);
