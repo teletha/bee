c62e7e8cbda5d61345fcbda773e3984d8d08e346
Teletha <Teletha.T@gmail.com>
2021-01-02 14:09:37 +0900

    Perform version downgrade on packaging instead of compiling.

--- src/main/java/bee/task/Compile.java
+++ src/main/java/bee/task/Compile.java
@@ -9,21 +9,12 @@
  */
 package bee.task;
 
-import java.io.ByteArrayInputStream;
-
-import javax.lang.model.SourceVersion;
-
 import bee.Bee;
 import bee.Task;
 import bee.api.Command;
 import bee.api.Scope;
-import bee.util.Inputs;
 import bee.util.JavaCompiler;
 import kiss.Signal;
-import net.bytebuddy.jar.asm.ClassReader;
-import net.bytebuddy.jar.asm.ClassVisitor;
-import net.bytebuddy.jar.asm.ClassWriter;
-import net.bytebuddy.jar.asm.Opcodes;
 import psychopath.Directory;
 
 public class Compile extends Task {
@@ -35,8 +26,7 @@ public class Compile extends Task {
      */
     @Command(value = "Compile main sources and copy other resources.", defaults = true)
     public void source() {
-        compile("main", project.getSourceSet(), project
-                .getClasses(), project.getJavaSourceVersion().compareTo(project.getJavaClassVersion()) > 0);
+        compile("main", project.getSourceSet(), project.getClasses());
     }
 
     /**
@@ -46,7 +36,7 @@ public class Compile extends Task {
      */
     @Command("Compile test sources and copy other resources.")
     public void test() {
-        compile("test", project.getTestSourceSet(), project.getTestClasses(), false);
+        compile("test", project.getTestSourceSet(), project.getTestClasses());
     }
 
     /**
@@ -56,7 +46,7 @@ public class Compile extends Task {
      */
     @Command("Compile project sources and copy other resources.")
     public void project() {
-        compile("project", project.getProjectSourceSet(), project.getProjectClasses(), false);
+        compile("project", project.getProjectSourceSet(), project.getProjectClasses());
     }
 
     /**
@@ -68,7 +58,7 @@ public class Compile extends Task {
      * @param input A source locations.
      * @param output A output location.
      */
-    private void compile(String type, Signal<Directory> input, Directory output, boolean modifyVersion) {
+    private void compile(String type, Signal<Directory> input, Directory output) {
         ui.talk("Copying ", type, " resources to ", output);
         input.to(dir -> {
             dir.observeCopyingTo(output, o -> o.glob("**", "!**.java").strip()).skipError().to();
@@ -83,88 +73,7 @@ public class Compile extends Task {
         compiler.setNoWarn();
         compiler.compile();
 
-        if (modifyVersion) {
-            String oldVersion = Inputs.normalize(project.getJavaSourceVersion());
-            String newVersion = Inputs.normalize(project.getJavaClassVersion());
-            ui.talk("Downgrade class version from ", oldVersion, " to ", newVersion, ".");
-
-            project.getClasses().walkFile("**.class").to(file -> {
-                ClassReader classReader = new ClassReader(file.bytes());
-                ClassWriter writer = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
-                ClassVisitor modification = new Modify(project.getJavaClassVersion(), writer);
-                classReader.accept(modification, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
-                file.write(new ByteArrayInputStream(writer.toByteArray()));
-            });
-        }
-
         // load project related classes
         Bee.load(project.getClasses());
-    }
-
-    /**
-     * 
-     */
-    private static class Modify extends ClassVisitor {
-
-        private final int version;
-
-        public Modify(SourceVersion ver, ClassVisitor classVisitor) {
-            super(Opcodes.ASM8, classVisitor);
-            switch (ver) {
-            case RELEASE_1:
-                version = Opcodes.V1_1;
-                break;
-            case RELEASE_2:
-                version = Opcodes.V1_2;
-                break;
-            case RELEASE_3:
-                version = Opcodes.V1_3;
-                break;
-            case RELEASE_4:
-                version = Opcodes.V1_4;
-                break;
-            case RELEASE_5:
-                version = Opcodes.V1_5;
-                break;
-            case RELEASE_6:
-                version = Opcodes.V1_6;
-                break;
-            case RELEASE_7:
-                version = Opcodes.V1_7;
-                break;
-            case RELEASE_8:
-                version = Opcodes.V1_8;
-                break;
-            case RELEASE_9:
-                version = Opcodes.V9;
-                break;
-            case RELEASE_10:
-                version = Opcodes.V10;
-                break;
-            case RELEASE_11:
-                version = Opcodes.V11;
-                break;
-            case RELEASE_12:
-                version = Opcodes.V12;
-                break;
-            case RELEASE_13:
-                version = Opcodes.V13;
-                break;
-            case RELEASE_14:
-                version = Opcodes.V14;
-                break;
-            default:
-                version = Opcodes.V15;
-                break;
-            }
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-            super.visit(this.version, access, name, signature, superName, interfaces);
-        }
     }
 }
--- src/main/java/bee/task/Jar.java
+++ src/main/java/bee/task/Jar.java
@@ -9,14 +9,22 @@
  */
 package bee.task;
 
+import java.io.ByteArrayInputStream;
 import java.util.jar.Attributes.Name;
+
+import javax.lang.model.SourceVersion;
 
 import bee.Task;
 import bee.api.Command;
 import bee.api.Library;
 import bee.api.Scope;
+import bee.util.Inputs;
 import kiss.I;
 import kiss.Signal;
+import net.bytebuddy.jar.asm.ClassReader;
+import net.bytebuddy.jar.asm.ClassVisitor;
+import net.bytebuddy.jar.asm.ClassWriter;
+import net.bytebuddy.jar.asm.Opcodes;
 import psychopath.Directory;
 import psychopath.File;
 import psychopath.Folder;
@@ -34,8 +42,9 @@ public class Jar extends Task {
     public void source() {
         require(Compile::source);
 
-        pack("main classes", I.signal(project.getClasses()), project.locateJar());
-        pack("main sources", project.getSourceSet(), project.locateSourceJar());
+        pack("main classes", I.signal(project.getClasses()), project
+                .locateJar(), project.getJavaSourceVersion().compareTo(project.getJavaClassVersion()) > 0);
+        pack("main sources", project.getSourceSet(), project.locateSourceJar(), false);
     }
 
     /**
@@ -50,8 +59,8 @@ public class Jar extends Task {
         File classes = project.getOutput().file(project.getProduct() + "-" + project.getVersion() + "-tests.jar");
         File sources = project.getOutput().file(project.getProduct() + "-" + project.getVersion() + "-tests-sources.jar");
 
-        pack("test classes", I.signal(project.getTestClasses()), classes);
-        pack("test sources", project.getTestSourceSet(), sources);
+        pack("test classes", I.signal(project.getTestClasses()), classes, false);
+        pack("test sources", project.getTestSourceSet(), sources, false);
     }
 
     /**
@@ -66,8 +75,8 @@ public class Jar extends Task {
         File classes = project.getOutput().file(project.getProduct() + "-" + project.getVersion() + "-projects.jar");
         File sources = project.getOutput().file(project.getProduct() + "-" + project.getVersion() + "-projects-sources.jar");
 
-        pack("project classes", I.signal(project.getProjectClasses()), classes);
-        pack("project sources", project.getProjectSourceSet(), sources);
+        pack("project classes", I.signal(project.getProjectClasses()), classes, false);
+        pack("project sources", project.getProjectSourceSet(), sources, false);
     }
 
     /**
@@ -79,7 +88,7 @@ public class Jar extends Task {
     public void document() {
         Directory output = require(Doc::javadoc);
 
-        pack("javadoc", I.signal(output), project.locateJavadocJar());
+        pack("javadoc", I.signal(output), project.locateJavadocJar(), false);
     }
 
     /**
@@ -91,8 +100,30 @@ public class Jar extends Task {
      * @param input
      * @param output
      */
-    private void pack(String type, Signal<Directory> input, File output) {
+    private void pack(String type, Signal<Directory> input, File output, boolean modifyVersion) {
         ui.talk("Build ", type, " jar: ", output);
+
+        if (modifyVersion) {
+            String oldVersion = Inputs.normalize(project.getJavaSourceVersion());
+            String newVersion = Inputs.normalize(project.getJavaClassVersion());
+            ui.talk("Downgrade class version from ", oldVersion, " to ", newVersion, ".");
+
+            input = input.map(dir -> {
+                Directory modified = Locator.temporaryDirectory();
+
+                dir.walkFile("**.class").to(file -> {
+                    File modifiedFile = modified.file(dir.relativize(file));
+                    System.out.println(modifiedFile);
+                    ClassReader classReader = new ClassReader(file.bytes());
+                    ClassWriter writer = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+                    ClassVisitor modification = new Modify(project.getJavaClassVersion(), writer);
+                    classReader.accept(modification, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
+                    modifiedFile.write(new ByteArrayInputStream(writer.toByteArray()));
+                });
+
+                return modified;
+            });
+        }
 
         Locator.folder().add(input, Option::strip).packTo(output);
     }
@@ -124,5 +155,72 @@ public class Jar extends Task {
             folder.add(library.getLocalJar().asArchive());
         }
         folder.packTo(output);
+    }
+
+    /**
+     * 
+     */
+    private static class Modify extends ClassVisitor {
+
+        private final int version;
+
+        public Modify(SourceVersion ver, ClassVisitor classVisitor) {
+            super(Opcodes.ASM8, classVisitor);
+            switch (ver) {
+            case RELEASE_1:
+                version = Opcodes.V1_1;
+                break;
+            case RELEASE_2:
+                version = Opcodes.V1_2;
+                break;
+            case RELEASE_3:
+                version = Opcodes.V1_3;
+                break;
+            case RELEASE_4:
+                version = Opcodes.V1_4;
+                break;
+            case RELEASE_5:
+                version = Opcodes.V1_5;
+                break;
+            case RELEASE_6:
+                version = Opcodes.V1_6;
+                break;
+            case RELEASE_7:
+                version = Opcodes.V1_7;
+                break;
+            case RELEASE_8:
+                version = Opcodes.V1_8;
+                break;
+            case RELEASE_9:
+                version = Opcodes.V9;
+                break;
+            case RELEASE_10:
+                version = Opcodes.V10;
+                break;
+            case RELEASE_11:
+                version = Opcodes.V11;
+                break;
+            case RELEASE_12:
+                version = Opcodes.V12;
+                break;
+            case RELEASE_13:
+                version = Opcodes.V13;
+                break;
+            case RELEASE_14:
+                version = Opcodes.V14;
+                break;
+            default:
+                version = Opcodes.V15;
+                break;
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+            super.visit(this.version, access, name, signature, superName, interfaces);
+        }
     }
 }
