fc57489cc7a93737c756c2002f6ab262894411f5
Teletha <Teletha.T@gmail.com>
2021-03-23 20:09:44 +0900

    test: Bee#load.

---
+++ src/test/java/bee/DynamicLoadTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2019 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package bee;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import kiss.I;
+import psychopath.Locator;
+
+class DynamicLoadTest {
+
+    @Test
+    void load() {
+        String fqcn = "com.github.teletha.AB";
+
+        Assertions.assertThrows(ClassNotFoundException.class, () -> I.type(fqcn));
+        Bee.load(Locator.file("src/test/resources/dynamic-load.jar"));
+        assert I.type(fqcn) != null;
+    }
+}
