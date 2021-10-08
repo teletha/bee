fc2df5b6fec7f507d5c9fdd2eaee3fe406a7bca5
Teletha <Teletha.T@gmail.com>
2021-10-08 09:22:00 +0900

    fix: Update maven resolver.

--- src/project/java/bee/Project.java
+++ src/project/java/bee/Project.java
@@ -11,7 +11,7 @@ package bee;
 
 public class Project extends bee.api.Project {
 
-    String resolver = "1.6.2";
+    String resolver = "1.7.2";
 
     {
         product(Bee.Tool.getGroup(), Bee.Tool.getProduct(), ref("version.txt"));
