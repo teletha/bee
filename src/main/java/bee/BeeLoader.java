3eaf9ba94eb79d424ad30252d817bcabb05c94b5
Teletha <Teletha.T@gmail.com>
2021-11-09 12:48:26 +0900

    revert

--- src/main/java/bee/BeeLoader.java
+++ src/main/java/bee/BeeLoader.java
@@ -43,19 +43,21 @@ public class BeeLoader {
      * @param path
      */
     public static synchronized void load(Location path) {
-        try {
-            if (path.isPresent()) {
-                // If you are not able to get instrumentation from JavaAgent, you can use
-                // bytebuddy-agent to get it dynamically. Since this method is only used during
-                // Bee development, the dependency of bytebuddy-agent is provided.
-                if (instrumentation == null) instrumentation = ByteBuddyAgent.install();
+        if (records.add(path)) {
+            try {
+                if (path.isPresent()) {
+                    // If you are not able to get instrumentation from JavaAgent, you can use
+                    // bytebuddy-agent to get it dynamically. Since this method is only used during
+                    // Bee development, the dependency of bytebuddy-agent is provided.
+                    if (instrumentation == null) instrumentation = ByteBuddyAgent.install();
 
-                if (path.isDirectory()) path = path.packToTemporary(Option::strip);
+                    if (path.isDirectory()) path = path.packToTemporary(Option::strip);
 
-                instrumentation.appendToSystemClassLoaderSearch(new JarFile(path.asJavaFile()));
+                    instrumentation.appendToSystemClassLoaderSearch(new JarFile(path.asJavaFile()));
+                }
+            } catch (IOException e) {
+                throw I.quiet(e);
             }
-        } catch (IOException e) {
-            throw I.quiet(e);
         }
     }
 }
