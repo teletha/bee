2d63a6aeec8547d5661d0eb788b5ed09d8f84f4d
Teletha <Teletha.T@gmail.com>
2021-11-15 22:34:00 +0900

    test installer

--- tool/install-bee.sh
+++ tool/install-bee.sh
@@ -1,3 +1,4 @@
 # To run this script remotely type this in your shell
 # 		curl -Ls https://git.io/install-bee | bash
-# 		bee
+curl -#L -o bee.jar https://jitpack.io/com/github/teletha/bee/0.13.0/bee-0.13.0.jar
+java -javaagent:bee.jar -cp bee.jar bee.BeeInstaller
