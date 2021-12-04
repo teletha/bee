b82dc9fd9802ff87791b072674375bb18fd6f35f
teletha <485441+teletha@users.noreply.github.com>
2021-12-04 20:05:41 +0900

    Update install-bee.sh

--- tool/install-bee.sh
+++ tool/install-bee.sh
@@ -1,5 +1,5 @@
 # To run this script remotely type this in your shell
 # 		curl -Ls https://git.io/install-bee | bash
 version=$(curl -SsL https://git.io/stable-bee)
-curl -#L -o bee.jar https://jitpack.io/com/github/teletha/bee/${version}/bee-${version}.jar
-java -javaagent:bee.jar -cp bee.jar bee.BeeInstaller
+curl -#L -o bee-${version}.jar https://jitpack.io/com/github/teletha/bee/${version}/bee-${version}.jar
+java -javaagent:bee-${version}.jar -cp bee-${version}.jar bee.BeeInstaller
