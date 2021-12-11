d206625f79e0cdccecab5f002d7a17fb18489482
Teletha <Teletha.T@gmail.com>
2021-12-12 00:15:20 +0900

    remove debug code

--- src/main/java/bee/Task.java
+++ src/main/java/bee/Task.java
@@ -12,7 +12,6 @@ package bee;
 import static org.objectweb.asm.Opcodes.*;
 
 import java.io.BufferedWriter;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
@@ -21,7 +20,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -408,22 +406,7 @@ public abstract class Task implements Extensible {
      * @param content A file content.
      */
     protected final File makeFile(File file, Iterable<String> content) {
-        try {
-            try (BufferedWriter writer = file.newBufferedWriter()) {
-                Iterator<String> iterator = content.iterator();
-                boolean hasNext = iterator.hasNext();
-                while (hasNext) {
-                    writer.append(iterator.next());
-
-                    hasNext = iterator.hasNext();
-                    if (hasNext) {
-                        writer.newLine();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new IOError(e);
-        }
+        file.text(content);
         ui.info("Make file [", file.absolutize(), "]");
 
         return file;
