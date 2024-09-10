63ee982f5df64d79ea01c19657f7dbada09e8e63
Teletha <Teletha.T@gmail.com>
2024-09-10 10:37:53 +0900

    feat: maven can download pom in parallel

--- src/main/java/bee/api/Repository.java
+++ src/main/java/bee/api/Repository.java
@@ -112,7 +112,9 @@ import org.eclipse.aether.internal.impl.checksum.Sha512ChecksumAlgorithmFactory;
 import org.eclipse.aether.internal.impl.checksum.SparseDirectoryTrustedChecksumsSource;
 import org.eclipse.aether.internal.impl.checksum.SummaryFileTrustedChecksumsSource;
 import org.eclipse.aether.internal.impl.checksum.TrustedToProvidedChecksumsSourceAdapter;
-import org.eclipse.aether.internal.impl.collect.FastDependencyCollector;
+import org.eclipse.aether.internal.impl.collect.DefaultDependencyCollector;
+import org.eclipse.aether.internal.impl.collect.bf.BfDependencyCollector;
+import org.eclipse.aether.internal.impl.collect.df.DfDependencyCollector;
 import org.eclipse.aether.internal.impl.filter.DefaultRemoteRepositoryFilterManager;
 import org.eclipse.aether.internal.impl.filter.GroupIdRemoteRepositoryFilterSource;
 import org.eclipse.aether.internal.impl.filter.PrefixesRemoteRepositoryFilterSource;
@@ -251,6 +253,7 @@ public class Repository {
         session.setSystemProperties(System.getProperties());
         session.setConfigProperties(System.getProperties());
         session.setConfigProperty("maven.artifact.threads", 24);
+        session.setConfigProperty("aether.dependencyCollector.impl", "bf");
 
         // event listener
         Loader transfers = I.make(Loader.class);
@@ -690,7 +693,7 @@ public class Repository {
         private Lifestyles() {
             define(RepositorySystem.class, DefaultRepositorySystem.class);
             define(ArtifactResolver.class, DefaultArtifactResolver.class, TrustedChecksumsArtifactResolverPostProcessor.class, GroupIdRemoteRepositoryFilterSource.class);
-            define(DependencyCollector.class, FastDependencyCollector.class);
+            define(DependencyCollector.class, DefaultDependencyCollector.class, BfDependencyCollector.class, DfDependencyCollector.class);
             define(MetadataResolver.class, DefaultMetadataResolver.class);
             define(Deployer.class, DefaultDeployer.class, SnapshotMetadataGeneratorFactory.class, VersionsMetadataGeneratorFactory.class);
             define(Installer.class, DefaultInstaller.class, SnapshotMetadataGeneratorFactory.class, VersionsMetadataGeneratorFactory.class);
