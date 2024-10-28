9dc55c12c5ccbe1d444d20faca15c21e023b8873
Teletha <Teletha.T@gmail.com>
2024-10-29 06:41:39 +0900

    wip

--- src/main/java/bee/task/Native.java
+++ src/main/java/bee/task/Native.java
@@ -31,17 +31,21 @@ import psychopath.Option;
 
 public class Native extends Task {
 
-    /** The location of graal builders. */
-    protected Directory jdk;
-
     /** The available protocols. default is 'http,https' */
     protected String protocols = "http,https";
 
+    /** The graal kind. */
+    private String kind = project.getDependency(Scope.Runtime).stream().anyMatch(lib -> lib.group.equals("org.openjfx")) ? "gluon"
+            : "graal";
+
+    /** The graal version. */
+    private int version = project.getJavaClassVersion().runtimeVersion().feature();
+
     /** The platform type. */
-    private String kind = "windows-x64";
+    private String target = detectOS() + "-" + detectArch();
 
     /** The configuration directory. */
-    private Directory config = project.getOutput().directory("native-config/" + kind + "-" + project.getVersion()).create();
+    private Directory config = project.getOutput().directory("native-config/" + kind + "-" + qualifier()).create();
 
     /** The output root. */
     private Directory output = project.getOutput().directory("native/" + qualifier()).create();
@@ -58,20 +62,21 @@ public class Native extends Task {
      * @return
      */
     private String qualifier() {
-        return project.getProduct() + "-" + kind + "-" + project.getVersion();
+        return project.getProduct() + "-" + target + "-" + project.getVersion();
     }
 
     @Command(value = "Build native execution file.", defaults = true)
     public File build() {
+        Directory graal = findGraalVM();
+
         // run test with native-image-agent
         Test test = I.make(Test.class);
-        test.java = findGraalVM(kind);
+        test.java = graal;
         test.params.add("-agentlib:native-image-agent=config-output-dir=" + config.path());
         test.test();
 
         require(Jar::source);
         String main = require(FindMain::main);
-        Directory graal = findGraalVM(kind);
 
         buildRuntimeInfo();
 
@@ -123,23 +128,15 @@ public class Native extends Task {
     /**
      * Detect GraalVM.
      * 
-     * @param platform
      * @return
      */
-    private Directory findGraalVM(String platform) {
-        if (jdk != null) {
-            return jdk;
-        }
-
-        boolean needJavaFX = project.getDependency(Scope.Runtime).stream().anyMatch(lib -> lib.group.equals("org.openjfx"));
-        int version = project.getJavaClassVersion().runtimeVersion().feature();
-
-        Directory dest = Platform.BeeHome.directory("jdk").directory((needJavaFX ? "gluon-" : "graal-") + version);
+    private Directory findGraalVM() {
+        Directory dest = Platform.BeeHome.directory("jdk").directory(kind + "-" + version);
 
         if (dest.isAbsent()) {
-            String url = needJavaFX
+            String url = kind.equals("gluon")
                     ? "https://github.com/gluonhq/graal/releases/download/gluon-23+25.1-dev-2409082136/graalvm-java23-windows-amd64-gluon-23+25.1-dev.zip"
-                    : "https://download.oracle.com/graalvm/" + version + "/archive/graalvm-jdk-" + version + "_" + platform + "_bin.zip";
+                    : "https://download.oracle.com/graalvm/" + version + "/archive/graalvm-jdk-" + version + "_" + target + "_bin.zip";
 
             // download archive
             File temp = Locator.temporaryFile(url.substring(url.lastIndexOf('/') + 1));
@@ -164,5 +161,33 @@ public class Native extends Task {
                 }
             }
         };
+    }
+
+    private static String detectOS() {
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.contains("win")) {
+            return "windows";
+        } else if (name.contains("nux") || name.contains("nix")) {
+            return "linux";
+        } else if (name.contains("mac")) {
+            return "macos";
+        } else {
+            throw new Error("Unknown OS [" + name + "]");
+        }
+    }
+
+    private static String detectArch() {
+        String name = System.getProperty("os.arch").toLowerCase();
+        if (name.contains("x86_64") || name.contains("amd64")) {
+            return "x64";
+        } else if (name.equals("x86") || name.equals("i386")) {
+            return "x86";
+        } else if (name.equals("aarch64") || name.equals("arm64")) {
+            return "aarch64";
+        } else if (name.equals("arm") || name.equals("arm32")) {
+            return "arm";
+        } else {
+            throw new Error("Unknown Architecture [" + name + "]");
+        }
     }
 }
