480a272af12866ba8780a5ed215de06e88fa59b9
Teletha <Teletha.T@gmail.com>
2025-04-07 21:37:12 +0900

    wip

--- src/main/java/bee/Bee.java
+++ src/main/java/bee/Bee.java
@@ -426,7 +426,7 @@ public class Bee {
         JEP483.enable(Locator.locate(Bee.class) + ".aot");
 
         // 3. Default task if none provided
-        if (tasks.length == 0) tasks = new String[] {"help"};
+        if (tasks.length == 0) tasks = new String[] {"install"};
 
         // 4. Parse command-line arguments into options and remaining tasks
         // Options (like --help, --root) are processed first.
